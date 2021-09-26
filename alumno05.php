ce6dbde5c046d69cf1f54b23c77855a0eec76533
Javier Boix <francisco.javier.boix.martinez@gmail.com>
2021-09-27 01:00:50 +0200

    Commit 7 - Creamos alumno05.php

---
+++ alumno05.php
@@ -0,0 +1,16 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
+<html>
+  <head>
+    <title>Alumno 05</title>
+  </head>
+  <body>
+    <p>P&aacute;gina de alumno 05</p>
+    <a href="index.html">Volver a P&aacute;gina Princial</a><br>
+    <?php
+    echo "Estamos en la Página de alumno 05\r\n";
+    ?>
+  </body>
+</html>
+
+
+
