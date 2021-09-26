4751db7bacb579e525028a65d130bb0d92778f05
Javier Boix <francisco.javier.boix.martinez@gmail.com>
2021-09-27 00:22:01 +0200

    Commit 001 - Subimos los archivos por primera vez

---
+++ alumno03.php
@@ -0,0 +1,16 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
+<html>
+  <head>
+    <title>Alumno 03</title>
+  </head>
+  <body>
+    <p>P&aacute;gina de alumno 03</p>
+    <a href="index.html">Volver a P&aacute;gina Princial</a><br>
+    <?php
+    echo "Estamos en la Página de alumno 03\r\n";
+    ?>
+  </body>
+</html>
+
+
+
